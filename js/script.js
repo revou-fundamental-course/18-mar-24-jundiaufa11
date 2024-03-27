7085e9c1b94d7b8748b087bece081bf26beda32d
jundi <jundiaufa26@gmail.com>
2024-03-27 13:15:37 +0700

    add slideshow

--- js/script.js
+++ js/script.js
@@ -45,3 +45,32 @@ function kirim() {
 
     return false;
 }
+
+// slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        
+    }
+
+    slides[slideIndex-1].style.display = "block";
+
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
